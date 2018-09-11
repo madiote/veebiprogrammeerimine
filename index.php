3305f0b92cfc30c36e49e156677f9f404e036ab5
Madis Otenurm <madiote@users.noreply.github.com>
2018-09-11 15:19:02 +0300

    Uuendasin PHP faili

--- index.php
+++ index.php
@@ -1,17 +1,52 @@
+<?php
+	//echo "See on minu esimene PHP!";
+	$firstName = "Madis";
+	$lastName = "Otenurm";
+	$dateToday = date("d.m.Y");
+	$hourNow = date("G");
+	$minuteNow = date("i");
+	$partOfDay = "";
+	
+	if ($hourNow < 8){
+		$partOfDay = "varajane hommik";
+	}
+	elseif ($hourNow >= 8 and $hourNow <= 16){
+		$partOfDay = "koolipäev";
+	}
+	elseif ($hourNow > 16){
+		$partOfDay = "vaba aeg";
+	}
+	else {
+		$partOfDay = "öö";
+	}
+?>
 <!DOCTYPE html>
 
 <html lang="et">
 	<head>
 		<meta charset="utf-8">
-		<title>Madis Otenurm, õppetöö</title>
+		<title>
+			<?php
+				echo $firstName;
+				echo " ";
+				echo $lastName;
+			?>, õppetöö
+		</title>
 		<link rel="icon" href="https://www.tlu.ee/themes/tlu/images/favicons/favicon-32x32.png" type="image/png" sizes="16x16">
 	</head>
 
 	<body>
-		<h1>Madis Otenurm</h1>
-		<p>Tere tulemast PHP lehele! <a href="../index.html">Tavasaidile saab siit</a></p><br />
+		<h1>
+			<?php
+				echo $firstName . " " . $lastName;
+			?>
+		</h1>
+		<p>Tere tulemast PHP lehele! <a href="../index.html">Tavasaidile saab siit</a></p>
+		<?php
+			echo "<p>Täna on " . $dateToday . " kell " . $hourNow . ":" . $minuteNow . " (" . $partOfDay . ").";
+		?><br />
 		<img src="../kool.jpg" alt="Laps kirjutab" width="500" height="500"><br />
-		<a href="../../~kellrei/index.html" target="_blank">Minu sõber teeb ka veebi</a><br />
+		<a href="../../~kellrei/veebiprogrammeerimine/index.php" target="_blank">Minu sõber teeb ka veebi (PHP)</a><br />
 		<p>Tasuta HTTPS kõigile! Lisa see oma lehele: </p>
 		<textarea rows="1" cols="50" readonly><script src="../~madiote/force-https.js"></script></textarea>
 	
