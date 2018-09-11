c1c3964a593a9a8050957edad43d7ff6a8f30f8a
Madis Otenurm <madiote@users.noreply.github.com>
2018-09-11 14:22:21 +0300

    Lisasin PHP faili

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="et">
+	<head>
+		<meta charset="utf-8">
+		<title>Madis Otenurm, õppetöö</title>
+		<link rel="icon" href="https://www.tlu.ee/themes/tlu/images/favicons/favicon-32x32.png" type="image/png" sizes="16x16">
+	</head>
+
+	<body>
+		<h1>Madis Otenurm</h1>
+		<p>Tere tulemast PHP lehele! <a href="../index.html">Tavasaidile saab siit</a></p><br />
+		<img src="../kool.jpg" alt="Laps kirjutab" width="500" height="500"><br />
+		<a href="../../~kellrei/index.html" target="_blank">Minu sõber teeb ka veebi</a><br />
+		<p>Tasuta HTTPS kõigile! Lisa see oma lehele: </p>
+		<textarea rows="1" cols="50" readonly><script src="../~madiote/force-https.js"></script></textarea>
+	
+	<footer>
+		<hr />
+		<p>See leht on valminud õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+		<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="Tallinna ülikool" width="300" height="150"> <a href="https://www.tlu.ee">TLÜ kodulehele saab siit</a>
+	</footer>
+	
+	<script src="force-https.js"></script> 
+	</body>
+</html>
