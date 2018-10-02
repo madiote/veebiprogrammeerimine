87d4d24b62d76ec19256c9cdd924e84de314f2ee
Madis Otenurm <madiote@users.noreply.github.com>
2018-10-02 14:16:02 +0300

    Tund5 + vormid

---
+++ tund5/addmsg.php
@@ -0,0 +1,50 @@
+<?php
+	require("functions.php");
+	$notice = null;
+	
+	if (isset($_POST["submitMessage"])){
+		if ($_POST["message"] != "Sõnum" and !empty($_POST["message"])){ // Check for placeholder or empty
+			$message = test_input($_POST["message"]);
+			$notice = saveamsg($message);
+		}
+		else {
+			$notice = "Palun kirjuta sõnum.";
+		}
+	}
+?>
+<!DOCTYPE html>
+
+<html lang="et">
+	<head>
+		<meta charset="utf-8">
+		<title>Anonüümse sõnumi lisamine</title>
+		<link rel="icon" href="https://www.tlu.ee/themes/tlu/images/favicons/favicon-32x32.png" type="image/png" sizes="16x16">
+	</head>
+
+	<body>
+		<h1>Sõnumi lisamine</h1>
+		<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			<label>Sõnum (max 256 märki):</label>
+			<br>
+			<textarea name="message" rows="4" cols="64" maxlength="256">Sõnum</textarea>
+			<br>
+			<input type="submit" name="submitMessage" value="Salvesta sõnum">
+		</form>
+		<p><?php echo $notice; ?></p>
+		
+		
+		<hr />
+		<div class="links"> <!-- Autogenerate list of links in current folder -->
+			<p>Selle tunni lehed:
+				<?php
+					$dirFiles = scandir("./");
+					for ($i = 2; $i < count($dirFiles); $i++){
+						echo '<a href="./' . $dirFiles[$i] . '">' . $dirFiles[$i]. '</a> ';
+					}
+				?>
+			</p>
+		</div>
+		<iframe src="../footer.html" frameBorder="0" height="auto" width="100%"></iframe>
+		<script src="../../force-https.js"></script> <!-- Force HTTPS with javascript -->
+	</body>
+</html>
---
+++ tund5/functions.php
@@ -0,0 +1,112 @@
+<?php
+	require("../../../config.php"); // Account details
+	$database = "if18_madis_ot_1";
+	
+	function saveamsg($msg){
+		$notice = "";
+		// Create db connection
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		// Preparing db statement
+		$stmt = $mysqli -> prepare("INSERT INTO vpamsg (message) VALUES(?)"); // Assign the command to mysqli, prepare statement for usage (without defined values)
+		echo $mysqli->error; // Echo a mysqli error if any
+		
+		// Replace the statement question marks with values; first types, then data
+		// s - string, i - integer, d - decimal
+		$stmt -> bind_param("s", $msg); // Similar to string.format(); but requires types to be defined first
+	
+		if($stmt -> execute()){ // Execute db statement, check if valid
+			$notice = 'Sõnum: "' . $msg . '" on edukalt salvestatud!';
+		}
+		else {
+			$notice = 'Sõnumi salvestamisel tekkis tõrge: ' . $stmt -> error;
+		}
+		
+		// Close the db
+		$stmt -> close();
+		// Close the connection
+		$mysqli -> close();
+		
+		return $notice;
+	}
+	
+	function readallmessages(){
+		$notice = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli -> prepare("SELECT message FROM vpamsg");
+		echo $mysqli->error;
+		
+		$stmt -> bind_result($msg);
+		if($stmt -> execute()){
+			// Not sure how to write this negatively, maybe by wrapping in parenthesis?
+		}
+		else {
+			$notice = 'Sõnumite saamisel tekkis tõrge: ' . $stmt -> error;
+		}
+		
+		while ($stmt -> fetch()){
+			$notice .= "<p>" . $msg . "</p> \n"; // .= means appending text
+		}
+		
+		$stmt -> close();
+		$mysqli -> close();
+		
+		return $notice;
+	}
+	
+	function createAndFetchCats($catName, $catColor, $catTail){
+		$notice = null;
+		$cats = null;
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		// Insert a cat
+		if ($catName != null and $catColor != null){ // Only if there is input
+			$stmt = $mysqli -> prepare("INSERT INTO kassid (nimi, v2rv, saba) VALUES(?, ?, ?)");
+			echo $mysqli->error;
+		
+			$stmt -> bind_param("ssi", $catName, $catColor, $catTail);
+	
+			if($stmt -> execute()){
+				$notice = 'Kass ' . $catName . ' on sisestatud!';
+			}
+			else {
+				$notice = 'Kassi sisestamisel esines tõrge: ' . $stmt -> error; // Never displayed, why?
+			}
+			$stmt -> close();
+		}
+		else { // Otherwise skip
+			$notice = "Kuvan tabelis olevaid kasse.";		
+		}
+
+		// Fetch the cats
+		$stmt = $mysqli -> prepare("SELECT nimi, v2rv, saba FROM kassid ORDER BY kiisu_id");
+		echo $mysqli->error;
+		
+		$stmt -> bind_result($readCatName, $readCatColor, $readCatTail);
+		if($stmt -> execute()){
+			// Do nothing if succeeded
+		}
+		else {
+			$notice = 'Kasside saamisel esines tõrge: ' . $stmt -> error; 
+		}
+		
+		while ($stmt -> fetch()){ // Too lazy for a 2-dimensional array, so separating with dashes
+			$cats .= "<li>" . $readCatName . " - " . $readCatColor . " - " . $readCatTail . "</li>";
+		}
+		
+		$stmt -> close();
+
+		$mysqli -> close();
+		return $cats . "|" . $notice; // Cannot return two variables properly
+	}
+
+	function test_input($data) { // Sanitize input
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+	
+	
+?>
